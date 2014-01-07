67b66842be29a1f2454db94c7a056b4c8e9a638f
Tim Wood <washwithcare@gmail.com>
2014-01-06 20:20:06 -0800

    Initial version

---
+++ test/main.js
@@ -0,0 +1,124 @@
+/*global describe, it*/
+"use strict";
+
+var fs = require("fs"),
+	es = require("event-stream"),
+	assert = require("assert"),
+	gutil = require("gulp-util"),
+	consolidate = require("../");
+
+describe("gulp-consolidate", function () {
+
+	it("should throw an error if the engine is not defined", function (done) {
+		assert.throws(function () {
+			consolidate();
+		});
+		done();
+	});
+
+	it("should throw an error if the engine is not loaded", function (done) {
+		assert.throws(function () {
+			consolidate("not-a-real-npm-module");
+		});
+		done();
+	});
+
+	it("should produce expected file via buffer", function (done) {
+		var srcFile = new gutil.File({
+			path: "test/fixtures/hello.txt",
+			cwd: "test/",
+			base: "test/fixtures",
+			contents: new Buffer("Hello {{name}}")
+		});
+
+		var expectedFile = new gutil.File({
+			path: "test/expected/hello.txt",
+			cwd: "test/",
+			base: "test/expected",
+			contents: new Buffer("Hello World")
+		});
+
+		var stream = consolidate("swig", {
+			name: "World"
+		});
+
+		stream.on("error", function (err) {
+			assert(err, "errors should throw");
+			done(err);
+		});
+
+		stream.on("data", function (newFile) {
+			assert(newFile, "new file should exist");
+			assert(newFile.contents, "new file contents should exist");
+
+			assert.equal(String(newFile.contents), String(expectedFile.contents), "file contents should match expected contents");
+			done();
+		});
+
+		stream.write(srcFile);
+		stream.end();
+	});
+
+	it("should use be able to load data from a callback", function (done) {
+		var srcFile = new gutil.File({
+			path: "test/fixtures/hello.txt",
+			cwd: "test/",
+			base: "test/fixtures",
+			contents: new Buffer("Hello {{path}}")
+		});
+
+		var expectedFile = new gutil.File({
+			path: "test/expected/hello.txt",
+			cwd: "test/",
+			base: "test/expected",
+			contents: new Buffer("Hello test/fixtures/hello.txt")
+		});
+
+		var stream = consolidate("swig", function (file) {
+			return {
+				path: file.path
+			};
+		});
+
+		stream.on("error", function (err) {
+			assert(err, "errors should throw");
+			done(err);
+		});
+
+		stream.on("data", function (newFile) {
+			assert(newFile, "new file should exist");
+			assert(newFile.contents, "new file contents should exist");
+
+			assert.equal(String(newFile.contents), String(expectedFile.contents), "file contents should match expected contents");
+			done();
+		});
+
+		stream.write(srcFile);
+		stream.end();
+	});
+
+	it("should error on stream", function (done) {
+		var srcFile = new gutil.File({
+			path: "test/fixtures/hello.txt",
+			cwd: "test/",
+			base: "test/fixtures",
+			contents: fs.createReadStream("test/main.js")
+		});
+
+		var stream = consolidate("swig");
+
+		stream.on("error", function (err) {
+			assert(err, "errors should throw");
+			done();
+		});
+
+		stream.on("data", function (newFile) {
+			newFile.contents.pipe(es.wait(function (err) {
+				done(err);
+			}));
+		});
+
+		stream.write(srcFile);
+		stream.end();
+	});
+});
